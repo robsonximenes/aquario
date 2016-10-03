47cdfcae3fd201204a3967ea9351c46ab9cd68ce
Robson Ximenes <robsonximenes@gmail.com>
2016-10-03 09:07:29 -0300

    Arquivos para cron

---
+++ lanterna.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "lanterna.sh: $(date +"%m/%d/%y %H:%M:%S")" >> ./aquario.log
---
+++ twinstar.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "twistar.sh: $(date +"%m/%d/%y %H:%M:%S")" >> /home/82546010549/Desktop/aquario/aquario.log
